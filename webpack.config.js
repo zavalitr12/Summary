bbc9aa0fbb83da65dbf182fbeeb9bd220f27f4c4
zavalitr12 <zavalitr@mail.ru>
2019-03-16 21:24:01 +0500

    Configured webpack for pug and scss

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        summary: "./src/index.js"
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                use: 'pug-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.pug'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+}
+
+
+
